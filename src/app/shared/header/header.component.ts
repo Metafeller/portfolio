77e33788b2407572787369d2a29a66cebd0eb1cc
Metafeller <savasboas@gmail.com>
2024-10-29 19:57:11 +0100

    add burger-menu and responsive basics

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  menuOpen = false;
 
   onMouseOver() {
     const logo = document.getElementById('header-logo') as HTMLImageElement;
@@ -21,6 +24,10 @@ export class HeaderComponent {
     if (logo) {
       logo.src = './../logos/beispiel-logo-default.png';
     }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 
 }
