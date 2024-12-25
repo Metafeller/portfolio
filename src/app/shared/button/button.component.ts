fbcde76cb9f47f7a00bbc382dd8c9100eb713762
Metafeller <savasboas@gmail.com>
2024-12-25 23:31:48 +0100

    add developer and branding components
    Ich habe die Basics für die Unterkomponenten (developer and branding) erstellt.

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -3,10 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Input() buttonClass: string = 'default-button'; // Standardklasse für Buttons
+  @Input() label: string = ''; // Label für den Button
+  @Input() type: string = 'button'; // Typ des Buttons (z. B. submit, reset, button)
 }
--- src/app/skills/developer/developer.component.ts
+++ src/app/skills/developer/developer.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-developer',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './developer.component.html',
   styleUrl: './developer.component.scss'
 })
 export class DeveloperComponent {
+  skills = [
+    { name: 'HTML', icon: 'assets/icons/html.svg', isHovered: false },
+    { name: 'CSS', icon: 'assets/icons/css.svg', isHovered: false },
+    { name: 'JavaScript', icon: 'assets/icons/javascript.svg', isHovered: false },
+    { name: 'Material Design', icon: 'assets/icons/material-design.svg', isHovered: false },
+    { name: 'TypeScript', icon: 'assets/icons/typescript.svg', isHovered: false },
+    { name: 'Angular', icon: 'assets/icons/angular.svg', isHovered: false },
+    { name: 'Firebase', icon: 'assets/icons/firebase.svg', isHovered: false },
+    { name: 'GIT', icon: 'assets/icons/git.svg', isHovered: false },
+    { name: 'Rest-API', icon: 'assets/icons/rest-api.svg', isHovered: false },
+    { name: 'Scrum', icon: 'assets/icons/scrum.svg', isHovered: false },
+    { name: 'Growth Mindset', icon: 'assets/icons/growth-mindset.svg', isHovered: false },
+  ];
 
+  showHover(skill: any): void {
+    skill.isHovered = true;
+  }
+
+  hideHover(skill: any): void {
+    skill.isHovered = false;
+  }
 }
+
