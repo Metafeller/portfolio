3774729859057d420518e7457e88fdacba09e456
Metafeller <savasboas@gmail.com>
2024-12-29 23:17:37 +0100

    add infobox overlays for skillset icons
    Ich habe die Infoboxen für die Skillset icons implementiert und werde noch einige Feinjustierungen vornehmen

--- src/app/skills/developer/developer.component.ts
+++ src/app/skills/developer/developer.component.ts
@@ -11,7 +11,14 @@ import { CommonModule } from '@angular/common';
 })
 export class DeveloperComponent {
   skills = [
-    { name: 'HTML', icon: '/icons/skillset/html.svg', isHovered: false },
+    { name: 'HTML', 
+      icon: '/icons/skillset/html.svg', 
+      isHovered: false, 
+      isInfoboxVisible: false, 
+      infoboxTitle: 'HTML',
+      infoboxDescription: 'The backbone of the web. Structure your content effectively.',
+      infoboxIcons: []
+    },
     { name: 'CSS', icon: '/icons/skillset/css.svg', isHovered: false },
     { name: 'JavaScript', icon: '/icons/skillset/javascript.svg', isHovered: false },
     { name: 'Material Design', icon: '/icons/skillset/material-design.svg', isHovered: false },
@@ -21,8 +28,33 @@ export class DeveloperComponent {
     { name: 'GIT', icon: '/icons/skillset/git.svg', isHovered: false },
     { name: 'Rest-API', icon: '/icons/skillset/rest-api.svg', isHovered: false },
     { name: 'Scrum', icon: '/icons/skillset/scrum.svg', isHovered: false },
-    { name: 'Growth Mindset', icon: '/icons/skillset/growth-mindset.svg', isHovered: false },
+    { 
+      name: 'Growth Mindset', 
+      icon: '/icons/skillset/growth-mindset.svg', 
+      isHovered: false, 
+      isInfoboxVisible: false, 
+      infoboxTitle: 'Growth Mindset',
+      infoboxDescription: 'I have a special interest in learning.',
+      infoboxIcons: [
+        { src: '/icons/skillset/react-color.svg', alt: 'React' },
+        { src: '/icons/skillset/vue-js-2-color.svg', alt: 'Vue.js' },
+        { src: '/icons/skillset/python-color-96.svg', alt: 'Python' },
+        { src: '/icons/skillset/django-50.svg', alt: 'Django' }
+      ]
+    },
   ];
+
+  toggleInfobox(skill: any): void {
+    // Andere Infoboxen schließen
+    this.skills.forEach(s => {
+      if (s !== skill) {
+        s.isInfoboxVisible = false;
+        s.isHovered = false; // Reset hover state for other skills
+      }
+    });
+    // Toggle the clicked infobox
+    skill.isInfoboxVisible = !skill.isInfoboxVisible;
+  }
 
   showHover(skill: any): void {
     skill.isHovered = true;
@@ -31,5 +63,10 @@ export class DeveloperComponent {
   hideHover(skill: any): void {
     skill.isHovered = false;
   }
+
+  // toggleInfobox(skill: any): void {
+  //   skill.isInfoboxVisible = !skill.isInfoboxVisible;
+  // }
+  
 }
 
