af8c3602bd73fc9f8523fabde27e5b56860dcd98
Metafeller <savasboas@gmail.com>
2025-02-03 23:48:52 +0100

    update contact.component.scss
    Ich passe das Design der Figma-Vorlage an.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
