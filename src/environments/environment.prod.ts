7761000b2eea06e7e99b2363cdd9cfdfc0f0fe54
Metafeller <savasboas@gmail.com>
2025-02-19 23:58:16 +0100

    delate test-API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     mailgun: {
-      apiKey: 'a016047966e48f82a52d6f602e5bc621-ac3d5f74-2a261c29',
+      apiKey: 'New API here',
       domain: 'mail.metafeller.com',
       endpoint: 'https://api.mailgun.net/v3/mail.metafeller.com/messages'
     }
