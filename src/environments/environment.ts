54749d05b8871b99e2c8c0b20fc498780ac94d8e
Metafeller <savasboas@gmail.com>
2025-02-20 21:37:30 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     mailgun: {
-      apiKey: 'New API here',
+      apiKey: 'Placeholder for New API here',
       domain: 'mail.metafeller.com',
       endpoint: 'https://api.mailgun.net/v3/mail.metafeller.com/messages'
     }
