338d3056579f36c6cbca798c7dff23d487737a1a
Metafeller <savasboas@gmail.com>
2025-02-25 23:16:06 +0100

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,16 @@
+// export const environment = {
+//   production: true,  // In `environment.ts` wird `false` sein
+//   resend: {
+//     apiKey: process.env['RESEND_API_KEY'] || '', // Wir setzen den API-Key als Umgebungsvariable mit .env
+//     domain: 'mail.metafeller.com',
+//     endpoint: 'https://api.resend.com/emails'
+//   }
+// };
+
 export const environment = {
-  production: true,  // In `environment.ts` wird `false` sein
-  resend: {
-    apiKey: process.env['RESEND_API_KEY'] || '', // Wir setzen den API-Key als Umgebungsvariable mit .env
-    domain: 'mail.metafeller.com',
-    endpoint: 'https://api.resend.com/emails'
+  production: true,
+  brevo: {
+    apiKey: '', // Hier wird der API-Key aus .env geladen
+    endpoint: 'https://api.brevo.com/v3/smtp/email'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,16 @@
+// export const environment = {
+//   production: false,  // In `environment.prod.ts` wird `true` sein
+//   resend: {
+//     apiKey: process.env['RESEND_API_KEY'] || '', // Wir setzen den API-Key als Umgebungsvariable mit .env
+//     domain: 'mail.metafeller.com',
+//     endpoint: 'https://api.resend.com/emails'
+//   }
+// };
+
 export const environment = {
-  production: false,  // In `environment.prod.ts` wird `true` sein
-  resend: {
-    apiKey: process.env['RESEND_API_KEY'] || '', // Wir setzen den API-Key als Umgebungsvariable mit .env
-    domain: 'mail.metafeller.com',
-    endpoint: 'https://api.resend.com/emails'
+  production: false,
+  brevo: {
+    apiKey: '', // Hier wird der API-Key aus .env geladen
+    endpoint: 'https://api.brevo.com/v3/smtp/email'
   }
 };
