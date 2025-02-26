25f9b5b1391c1ef4485027c627cce972b503226c
Metafeller <savasboas@gmail.com>
2025-02-26 19:33:14 +0100

    clean code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,3 @@
-// export const environment = {
-//   production: false,  // In `environment.prod.ts` wird `true` sein
-//   resend: {
-//     apiKey: process.env['RESEND_API_KEY'] || '', // Wir setzen den API-Key als Umgebungsvariable mit .env
-//     domain: 'mail.metafeller.com',
-//     endpoint: 'https://api.resend.com/emails'
-//   }
-// };
-
 export const environment = {
   production: false,
   brevo: {
