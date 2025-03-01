c02d1900bdd1fd5f92abdd7448dfffe6beb699b9
Metafeller <savasboas@gmail.com>
2025-03-01 21:04:21 +0100

    add new service feature

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   brevo: {
-    // apiKey: process.env['BREVO_API_KEY'], // API-Key aus .env laden
     apiKey: 'Platzhalter Brevo API',
     endpoint: 'https://api.brevo.com/v3/smtp/email'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   brevo: {
-    // apiKey: process.env['BREVO_API_KEY'], // API-Key aus .env laden
     apiKey: 'Platzhalter Brevo API',
     endpoint: 'https://api.brevo.com/v3/smtp/email'
   }
