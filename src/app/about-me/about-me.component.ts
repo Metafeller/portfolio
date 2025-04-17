fc8a6d11327005d003ef658c22eed54e56b02bb6
Metafeller <savasboas@gmail.com>
2025-04-17 11:35:33 +0200

    add about-me.component slide-in basics
    Ich habe die Slide-In Animation integriert, aber ich werde heute Abend nochmal daran etwas Feilen. Es gefällt mir noch nicht ganz.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, Renderer2, ElementRef } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -8,6 +9,37 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observeElements(); // Beste Stelle für DOM-Animationen!
+    }
+  }
+
+  observeElements(): void {
+    const elements = this.elRef.nativeElement.querySelectorAll(
+      '.about-me-image-wrapper, .about-me-text, .about-me-box li'
+    );
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(entry.target, 'in-view');
+            observer.unobserve(entry.target); // Einmal auslösen
+          }
+        });
+      },
+      { threshold: 0.3 }
+    );
+
+    elements.forEach((el: HTMLElement) => observer.observe(el));
+  }
 }
