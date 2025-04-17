0db34987164e924e0ec9a3f5ee2844ba6f527c6d
Metafeller <savasboas@gmail.com>
2025-04-17 23:20:24 +0200

    add hero slide animation
    Ich habe das Fundament für die Slide Animation in der Hero Component implementiert. Es sieht bereits gut aus. Ich werde noch leichte Anpassungen machen.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ScrollService } from '../services/scroll.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { 
+  AfterViewInit, 
+  ElementRef, 
+  Inject, 
+  PLATFORM_ID, 
+  Renderer2 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Component({
@@ -11,9 +18,38 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
 
-  constructor(private scrollService: ScrollService) {}
+  constructor(
+    private scrollService: ScrollService,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.observeElements();
+    }
+  }
+
+  observeElements(): void {
+    const elements = this.elRef.nativeElement.querySelectorAll('.slide-in-left, .slide-in-right, .fade-in');
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(entry.target, 'in-view');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    elements.forEach((el: HTMLElement) => observer.observe(el));
+  }
 
   navigateTo(section: string) {
     this.scrollService.navigateToSection(section);
