afe246e05dda1e75dc682973b4fac5cfce5845cb
Metafeller <savasboas@gmail.com>
2025-04-24 19:19:34 +0200

    update cookie-banner (finish)
    Ich habe jetzt den Cookie-Banner komplett fertig gemacht. Die Verlinkungen zu Google Analytics und Co. werden noch folgen.

--- src/app/cookie-banner/cookie-banner.component.ts
+++ src/app/cookie-banner/cookie-banner.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cookie-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cookie-banner.component.html',
   styleUrl: './cookie-banner.component.scss'
 })
 export class CookieBannerComponent implements OnInit {
   showBanner = false;
-  localStorageKey = 'cookieConsent';
+  showSettings = false;
 
-  // Plattform-Check, ob Code im Browser läuft
+  analyticsConsent = true;
+  marketingConsent = true;
+
+  localStorageKey = 'cookieConsent';
   private platformId = inject(PLATFORM_ID);
 
   ngOnInit(): void {
@@ -46,6 +50,18 @@ export class CookieBannerComponent implements OnInit {
   }
 
   openSettings() {
-    alert('Settings-Modal kommt gleich 😎');
+    this.showSettings = true;
+  }
+
+  saveSettings() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(this.localStorageKey, JSON.stringify({
+        necessary: true,
+        analytics: this.analyticsConsent,
+        marketing: this.marketingConsent
+      }));
+      this.showBanner = false;
+      this.showSettings = false;
+    }
   }
 }
