fd5397bc1dc71da3bc9c6e552d19f2e5c1d8c9e9
Metafeller <savasboas@gmail.com>
2025-05-19 20:48:59 +0200

    update footer locations

--- src/app/shared/footer/footer.config.service.ts
+++ src/app/shared/footer/footer.config.service.ts
@@ -80,6 +80,7 @@ export class FooterConfigService {
         // { icon: '/svg-icons/nation/gran-canaria-islands.png', text: 'Gran Canaria', link: 'https://savasboas.com/blog/locations' },
         // { icon: '/svg-icons/nation/vereinigte-arabische-emirate.png', text: 'Dubai (VAE)', link: '#' },
         { icon: '/svg-icons/nation/northern-cyprus.png', text: 'Northern Cyprus', link: 'https://savasboas.com/blog/locations' },
+        // { icon: '/svg-icons/nation/sardinia.png', text: 'Sardinia', link: 'https://savasboas.com/blog/locations' },
         { icon: '/svg-icons/media/icons8-avengers-100.svg', text: 'Razor-Point', link: 'https://savasboas.com/blog/locations' }
       ],
       followMeOn: [
